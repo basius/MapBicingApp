43dad0938026a15d691f35c3c6813badb78809cd
basius <jorabada@gmail.com>
2017-02-07 12:50:50 +0100

    Creem objecte MapView al MainActivityFragment. Afegim MapView de OSM al layout

--- app/src/main/java/com/example/basius/mapbicingapp/MainActivityFragment.java
+++ app/src/main/java/com/example/basius/mapbicingapp/MainActivityFragment.java
@@ -6,17 +6,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.MapView;
+
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
-
+    MapView map;
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        map = (MapView) view.findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.HIKEBIKEMAP);
+        map.setTilesScaledToDpi(true);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+        return view;
     }
 }
